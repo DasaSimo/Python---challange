81b41c81901c11e7da1b979c3ffad45b54a96883
Dasha <84484371+DasaSimo@users.noreply.github.com>
2021-06-17 23:06:54 -0400

    PyBank month count

---
+++ PyBank/main.py
@@ -0,0 +1,13 @@
+#The total number of months 
+
+
+
+#The net total amount of "Profit/Losses" 
+
+# the changes in "Profit/Losses" over the entire period, 
+#    - the average of those changes
+
+#The greatest increase in profits (date and amount)
+
+#The greatest decrease in profits (date and amount) 
+
