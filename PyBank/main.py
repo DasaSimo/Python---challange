f88aaae94a86168dfc659a077c041ca65960bdfb
Dasha <84484371+DasaSimo@users.noreply.github.com>
2021-06-19 17:40:56 -0400

    completed - added remaing funcionality (greatest increase and decrease) created output text file

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,9 +9,14 @@ csvpath = os.path.join("Resources", "budget_data.csv")
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    # Loop through looking for number of the month
     month_count = 0
     total = 0
+    change = 0 
+    prev_PL = 0
+    tot_change = 0
+    max_inc_value = 0
+    max_dec_value = 0
+
     #Skip the header
     next(csvreader, None)
 
@@ -20,37 +25,43 @@ with open(csvpath) as csvfile:
         month_count += 1
         #The net total amount of "Profit/Losses"
         total += int(row[1])
+        # calculation for the first row and the rest 
+        if month_count ==1:
+            prev_PL = int(row[1])      
+        else:
+            change = int(row[1]) - prev_PL
+            tot_change += change
+            prev_PL = int(row[1])
+           # looking for maximum values in change and taking the date for the maximum found 
+            if change  > max_inc_value:
+                max_inc_value = change
+                max_inc_date = row[0]
+            if change < max_dec_value:
+                max_dec_value= change
+                max_dec_date = row[0]
+
+    # calculation for average change, round up to 2 decimal places
+    average_change = round(tot_change / (month_count - 1), 2)
     
 
+
+# printing final statement 
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {month_count}") 
 print(f"Total: ${total}")
-
-print("Average  Change: $-2315.12")
-print("Greatest Increase in Profits: Feb-2012 ($1926159)")
-print("Greatest Decrease in Profits: Sep-2013 ($-2196167)")
-
+print(f"Average  Change: ${average_change}")
+print(f"Greatest Increase in Profits: {max_inc_date}  $({max_inc_value})")
+print(f"Greatest Decrease in Profits: {max_dec_date}  $({max_dec_value})")
 
 
-
-  
-
-# the changes in "Profit/Losses" over the entire period, 
-#    - the average of those changes
-
-#The greatest increase in profits (date and amount)
-
-#The greatest decrease in profits (date and amount) #The total number of months 
-
-
-
-#The net total amount of "Profit/Losses" 
-
-# the changes in "Profit/Losses" over the entire period, 
-#    - the average of those changes
-
-#The greatest increase in profits (date and amount)
-
-#The greatest decrease in profits (date and amount) 
-
+# Open the file and write text
+with open("Financial_analysis.txt", 'w' ) as textfile:
+    print("Financial Analysis", file = textfile)
+    print("----------------------------", file = textfile)
+    print(f"Total Months: {month_count}", file = textfile) 
+    print(f"Total: ${total}", file = textfile)
+    print(f"Average  Change: ${average_change}", file = textfile)
+    print(f"Greatest Increase in Profits: {max_inc_date}  $({max_inc_value})", file = textfile)
+    print(f"Greatest Decrease in Profits: {max_dec_date}  $({max_dec_value})", file = textfile)
+   
