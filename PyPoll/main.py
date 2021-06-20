45d52a6cf24fc3c196f916e9f9786c98473b7182
Dasha <84484371+DasaSimo@users.noreply.github.com>
2021-06-20 19:17:29 -0400

    calculation for percentage added, winner by  popular vote (maximum) added print for final statement added

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,9 +6,6 @@ votes_count  = 0
 candidates = []
 cand_votes = []
 
-
-
-
 # Set path for file
 csvpath = os.path.join("Resources", "election_data.csv")
 
@@ -20,52 +17,54 @@ with open(csvpath) as csvfile:
     next(csvreader, None)
 
 
-
     for row in csvreader:
-         #* The total number of votes cast
+         # The total number of votes cast
         votes_count += 1
         name = row[2]
+        # Finding candidates with corresponding numbers of votes (in two lists):
         if name in candidates:
             position = candidates.index(name)
             cand_votes[position] += 1
         else:
             candidates.append(name)
             cand_votes.append(1) 
+   
+
+#  finding the winner of the election based on popular vote - maximum count of votes
+max_count = max(cand_votes)
+position = cand_votes.index(max_count)
 
 
-
-
-
-
-
-# * A complete list of candidates who received votes
-
-#* The percentage of votes each candidate won
-
-#  * The total number of votes each candidate won
-
-#  * The winner of the election based on popular vote.
-
-#* As an example, your analysis should look similar to the one below:
-
-#  Election Results
-#  -------------------------
-#  Total Votes: 3521001
-#  -------------------------
-#  Khan: 63.000% (2218231)
-#  Correy: 20.000% (704200)
-#  Li: 14.000% (492940)
-#  O'Tooley: 3.000% (105630)
-#  -------------------------
-#  Winner: Khan
-#  -------------------------
-
-#* In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
 # printing final statement 
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes:  {votes_count}")   
 print("-------------------------")
-print(candidates)
-print(cand_votes)
+
+print(f"Complete list of candidates:")
+# The percentage of votes each candidate won
+for i in range(len(candidates)):
+        print(f"{candidates[i]}: {round(cand_votes[i]/votes_count *100 ,2)}%    ({cand_votes[i]} votes)")
+
+#The winner of the election
+print("-------------------------")
+print(f"Winner: {candidates[position]}") 
+print("-------------------------")
+
+# Opening the new file and writing text
+with open("Election Results.txt", 'w' ) as textfile:
+        print("Election Results", file = textfile)
+        print("----------------------------", file = textfile)
+        print(f"Total Votes:  {votes_count}", file = textfile)   
+        print("-------------------------", file = textfile)
+
+        print(f"Complete list of candidates:", file = textfile)
+    # The percentage of votes each candidate won
+        for i in range(len(candidates)):
+            print(f"{candidates[i]}: {round(cand_votes[i]/votes_count *100 ,2)}%    ({cand_votes[i]} votes)", file = textfile)
+
+    #The winner of the election
+        print("-------------------------", file = textfile)
+        print(f"Winner: {candidates[position]}", file = textfile) 
+        print("-------------------------", file = textfile)
